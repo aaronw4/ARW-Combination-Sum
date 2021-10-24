cd9a8c071add66285f2446d8448f41c21fc13d91
Aaron Williams <aaronrw4@yahoo.com>
2021-10-24 16:40:26 -0700

    Solution to combination sums 2

---
+++ combinations2.js
@@ -0,0 +1,38 @@
+var combinationSum = function(candidates, target) {
+    let solutions = []
+    let sortedCand = candidates.sort((a, b) => a - b)
+
+    function findSum(numsPicked, numsRem, target) {
+        for (var i = 0; i < numsRem.length; i++) {
+            let sum
+
+            if (numsRem[i] === numsRem[i-1]) {
+                continue
+            }
+
+            if (numsPicked.length === 0) {
+                sum = numsRem[i]
+            } else {
+                sum = numsPicked.reduce((a, b) => a + b) + numsRem[i]
+            }
+
+            if (sum === target) {
+                numsPicked.push(numsRem[i])
+                solutions.push(numsPicked)
+                break
+            } else if (sum < target) {
+                let newNumsPicked = [...numsPicked]
+                newNumsPicked.push(numsRem[i])
+                let newNumsRem = numsRem.slice(i+1)
+                findSum(newNumsPicked, newNumsRem, target)
+            } else {
+                break
+            }
+        }
+    }
+
+    findSum([], sortedCand, target)
+    return solutions
+};
+
+console.log(combinationSum([2,5,2,1,2], 5))
